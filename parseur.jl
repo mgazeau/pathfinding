bf0a2041fb557e0ba3dafab2baad6b7f34e54d94
mgazeau <162680768+mgazeau@users.noreply.github.com>
2024-03-15 18:46:23 +0100

    Add files via upload

---
+++ parseur.jl
@@ -0,0 +1,41 @@
+using FileIO
+
+"""
+Fonction qui transforme le contenu d'un fichier map comprenant les caractère {'@','T','.','W','S'} en matrice d'entier 
+"""
+function lire_fichier_map(nom_fichier::String)
+
+    # Recupèration du contenu du fichier
+    fichier = open(nom_fichier, "r")
+    contenu::String = read(fichier, String)
+    close(fichier)
+
+    # Divise le contenu en lignes
+    lignes::Array{String} = split(contenu, '\n')
+
+    # Recupèration de la hauteur et de la largeur de la carte
+    hauteur::Int64, largeur::Int64 = parse(Int, split(lignes[2])[2]), parse(Int, split(lignes[3])[2])
+
+    # Initialise la matrice avec des zéros
+    matrice::Matrix{Int64} = zeros(Int, hauteur, largeur)
+
+    # Remplit la matrice en fonction des caractères du fichier .map
+    for i in 1:hauteur
+        for (j, char) in enumerate(lignes[4 + i])
+            if char == '@'
+                matrice[i, j] = -1  
+            elseif char == 'T'
+                matrice[i, j] = 0   
+            elseif char == '.'
+                matrice[i, j] = 1 
+            elseif char == 'S'
+                matrice[i, j] = 5 
+            elseif char == 'W'
+                matrice[i, j] = 8 
+            end
+        end
+    end
+
+    return matrice
+end
+
---
+++ recherche_aveugle.jl
@@ -0,0 +1,80 @@
+"""
+Algorithme de recherche aveugle d'un chemin entre la case de depart et d'arrivee en inondant la carte depuis le point de départ
+"""
+function flood_fill(carte::Matrix{Int64}, depart::Tuple{Int64,Int64}, arrivee::Tuple{Int64,Int64})
+    
+    #Pour compter le nombre de case visité
+    i::Int64 = 0
+    
+    m::Int64, n::Int64 = size(carte)
+
+    directions::Vector{Tuple{Int64,Int64}} = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    
+    #Vecteur qui représente l'ensemble des chemins en cours de traitement tel que chaque élement corresponde : la case actuelle, la distance entre la case actuelle et le depart, le vecteur des cases par lesquelles le chemin est passé depuis le depart 
+    file::Vector{Tuple{Tuple{Int64, Int64}, Int64, Vector{Tuple{Int64, Int64}}}} = [(depart, 0, Vector{Tuple{Int, Int}}(undef, 0))]
+    
+    #Ensemble des cases déjà visitées
+    visited::Set{Tuple{Int, Int}} = Set{Tuple{Int, Int}}()
+
+
+    while !isempty(file)
+    	
+        (x::Int64, y::Int64), distance::Int64, chemin::Vector{Tuple{Int64,Int64}} = popfirst!(file)
+
+	#Condition d'arrêt : la case actuelle est la case d'arrivée
+        if (x, y) == arrivee
+            println("Nombre d'operations : $i")
+            return chemin
+        end
+	
+	#Si le point regardé est une case non praticable on passe au prochain chemin
+        if (x, y) in visited || carte[x, y] == 0 || carte[x, y] == -1 
+            continue
+        end
+
+	#La case est ajouté aux case visitées
+        push!(visited, (x, y))
+	
+	#On regarde les 4 cases voisines
+        for (dx::Int64, dy::Int64) in directions
+            n_x, n_y = x + dx, y + dy
+
+            if 1 <= n_x <= m && 1 <= n_y <= n && (carte[n_x, n_y] >= 1 )
+            	i=i+1 #On compte une opération
+            	#Si la case voisine est praticable alors on ajoute dans la file de de traitement de nos chemins
+                push!(file, ((n_x, n_y), distance + 1, vcat(chemin, [(n_x, n_y)])))
+            end
+        end
+    end
+    
+    return []  # Pas de chemin trouvé
+end
+
+
+function recherche_aveugle(fichier::String,depart::Tuple{Int64,Int64}, arrivee::Tuple{Int64,Int64})
+	carte::Matrix{Int64} = lire_fichier_map(fichier)
+	
+        getTime = time()
+	
+	chemin::Vector{Tuple{Int64,Int64}} = flood_fill(carte,depart,arrivee)
+	
+	temps = round(time() - getTime,digits=6)
+	
+	if chemin != []
+        #Affichage des données
+	    println("Chemin obtenu avec flood_fill : $chemin")
+	    taille = length(chemin)
+	    println("Taille du chemin $taille")
+	    println("Temps d'execution $temps")
+
+        #création de l'image avec le chemin
+        image = matrice_to_image(carte, chemin, arrivee, depart)
+        save_image(image,"tests/chemin_aveugle.png")
+	else
+	    println("Pas de chemin possible")
+	end
+end
+
+
+
+
